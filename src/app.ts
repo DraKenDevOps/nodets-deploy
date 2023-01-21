63de7adbfcc5ed8f7cd141cae6112152cb1ddd60
champadev <sonephet.dev@gmail.com>
2023-01-21 08:52:04 +0700

    FEAT: deploy

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+const app = express()
+
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    return res.json("Hello world!")
+})
+
+app.listen(3344, () => {
+    console.log('listening on port 3344')
+})
+
